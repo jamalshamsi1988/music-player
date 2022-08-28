ebf65cced410833a97195bbe5aa03e201834d378
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-08-28 19:05:21 +0430

    added style

--- music-app/src/Components/Header.jsx
+++ music-app/src/Components/Header.jsx
@@ -1,19 +1,22 @@
-import logo from "./../assets/img/download-sound.png";
-import musicImg from "./../assets/img/music-img.jpg";
+import logo from "./../assets/img/right-img.jpg";
+import musicImg from "./../assets/img/image-background1.jpg";
 
 const Header = () => {
   return (
     <header>
       <article className="container h-100">
         <article className="h-100 d-flex align-items-center">
-          <section className="music-cover " style={{bacgroundImage:`url(${musicImg})`}}>
+          <section
+            className="music-cover  d-flex justify-content-center"
+            style={{ backgroundImage: `url(${musicImg})` }}
+          >
             <section className="filter-cover">
               <svg
+                className="player-svg"
                 xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
+                width="100"
+                height="100"
                 fill="currentColor"
-                class="bi bi-play-circle"
                 viewBox="0 0 16 16"
               >
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -23,25 +26,28 @@ const Header = () => {
           </section>
 
           <section className="music-description flex-grow-1">
-            <section>
+            <section className="d-flex justify-content-between">
               <section className="music-title">
-                <h2>Name Ahang</h2>
-                <h3>Name singer</h3>
+                <h1>Name Ahang</h1>
+                <h2>Name singer</h2>
               </section>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                width="30"
+                height="30"
                 fill="currentColor"
-                class="bi bi-star"
                 viewBox="0 0 16 16"
               >
                 <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
               </svg>
             </section>
             <section className="music-range">
-              <span>00:35</span> <span>03:35</span>
-              <input type="range" />
+              <section className="d-flex justify-content-between">
+                <span className="current-time">00:35</span>
+                <span className="full-time">03:35</span>
+              </section>
+
+              <input className="w-100" type="range" />
             </section>
             <section className="music-nav">
               <button className="btn">
@@ -50,33 +56,30 @@ const Header = () => {
                   width="30"
                   height="30"
                   fill="currentColor"
-                  class="bi bi-caret-left-square"
                   viewBox="0 0 16 16"
                 >
                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
                   <path d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082z" />
                 </svg>
               </button>
-              <button>
+              <button className="btn">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
                   height="30"
                   fill="currentColor"
-                  class="bi bi-play-btn"
                   viewBox="0 0 16 16"
                 >
                   <path d="M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
                   <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm15 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
                 </svg>{" "}
               </button>
-              <button>
+              <button className="btn">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
                   height="30"
                   fill="currentColor"
-                  class="bi bi-caret-right-square"
                   viewBox="0 0 16 16"
                 >
                   <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
@@ -85,8 +88,13 @@ const Header = () => {
               </button>
             </section>
           </section>
-          <section className="main-logo">
-            <img src={logo} width="80" height="80" />
+          <section className="main-logo h-100 pt-5 align-items-strat w-25 d-flex justify-content-end">
+            <img
+              src={logo}
+              width="150"
+              height="150"
+              style={{ borderRadius: "50%" }}
+            />
           </section>
         </article>
       </article>
